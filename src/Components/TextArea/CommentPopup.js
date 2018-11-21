1e4c0f9679cc4863463268b038f1a2f4b862b8c6
Ankit Kumar <aggarwal.ankit@magicedtech.com>
2018-11-21 17:37:46 +0530

    Completed

--- src/Components/TextArea/CommentPopup.js
+++ src/Components/TextArea/CommentPopup.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react"
 import { connect } from 'react-redux'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 class CommentPopup extends Component {
     constructor(props) {
@@ -29,19 +28,14 @@ class CommentPopup extends Component {
     }
     render() {
         return (
-            <ReactCSSTransitionGroup
-                transitionName="example"
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={300}>
-                <div className="comment-popup" style={{ display: this.props.display ? "inline" : "none", color: "white", background: "gray", position: "fixed", margin: "300px 0px 0px 500px", padding: "10px" }}>
-                    Please enter a note<br />
+            <div className="comment-popup" style={{ display: this.props.display ? "inline" : "none", color: "white", background: "gray", position: "fixed", margin: "300px 0px 0px 500px", padding: "10px" }}>
+                Please enter a note<br />
 
-                    <textarea value={this.state.comment} onChange={this.onChange} rows="5" cols="80" placeholder="Type..." />
-                    <br />
-                    <button className="save" onClick={this.onClick}>Save</button>{" "}
-                    <button className="cancel" onClick={this.onClick}>Cancel</button>
-                </div>
-            </ReactCSSTransitionGroup>
+                <textarea value={this.state.comment} onChange={this.onChange} rows="5" cols="80" placeholder="Type..." />
+                <br />
+                <button className="save" onClick={this.onClick}>Save</button>{" "}
+                <button className="cancel" onClick={this.onClick}>Cancel</button>
+            </div>
         )
     }
 } export default connect()(CommentPopup)
